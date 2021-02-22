5483d400150d85d4aac63ba5685f595e2a8bc30c
Mateus Peixoto <mateuspeixoto.contato@gmail.com>
2021-02-22 13:43:46 -0300

    atributos parte 2

--- educaUS/src/main/java/com/educaUS/educaUS/model/Postagem.java
+++ educaUS/src/main/java/com/educaUS/educaUS/model/Postagem.java
@@ -2,6 +2,7 @@ package com.educaUS.educaUS.model;
 
 import java.util.Date;
 
+import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -36,6 +37,76 @@ public class Postagem {
 	@NotNull
 	@Size(min = 1, max = 250)
 	private String comentario;
+	
+	private Long fk_id_usuario;
+	
+	private Long fk_id_tema;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getPostdate() {
+		return postdate;
+	}
+
+	public void setPostdate(Date postdate) {
+		this.postdate = postdate;
+	}
+
+	public int getVisualizacao() {
+		return visualizacao;
+	}
+
+	public void setVisualizacao(int visualizacao) {
+		this.visualizacao = visualizacao;
+	}
+
+	public int getCurtida() {
+		return curtida;
+	}
+
+	public void setCurtida(int curtida) {
+		this.curtida = curtida;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+	public Long getFk_id_usuario() {
+		return fk_id_usuario;
+	}
+
+	public void setFk_id_usuario(Long fk_id_usuario) {
+		this.fk_id_usuario = fk_id_usuario;
+	}
+
+	public Long getFk_id_tema() {
+		return fk_id_tema;
+	}
+
+	public void setFk_id_tema(Long fk_id_tema) {
+		this.fk_id_tema = fk_id_tema;
+	}
+	
+	
 	
 	
 	
