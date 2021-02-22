1a868109078717ce03797fd23006388c4380bbac
Lucas Pereira <pereira.lucas23@gmail.com>
2021-02-22 14:16:20 -0300

    Model usuário

---
+++ educaUS/src/main/java/com/educaUS/educaUS/model/usuario.java
@@ -0,0 +1,67 @@
+package com.educaUS.educaUS.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table (name = "usuario")
+public class usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_usuario;
+	
+	@NotNull
+	@Size (min = 5, max = 100)
+	private String nome;
+	
+	@NotNull
+	@Size (min = 5, max = 50)
+	private String email;
+	
+	@NotNull
+	@Size (min = 5, max = 50)
+	private String senha;
+	
+	
+	public long getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(long id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	
+
+}
