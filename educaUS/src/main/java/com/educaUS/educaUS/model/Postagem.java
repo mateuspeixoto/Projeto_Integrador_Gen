4f71df6469d56082589e1f975508e85a5ae186bf
unknown <alexandre2010nogueira@hotmail.com>
2021-02-22 13:28:13 -0300

    Criando tabela postagem e seus atributos

--- educaUS/src/main/java/com/educaUS/educaUS/model/Postagem.java
+++ educaUS/src/main/java/com/educaUS/educaUS/model/Postagem.java
@@ -1,5 +1,57 @@
 package com.educaUS.educaUS.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name= "postagem")
 public class Postagem {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date postdate = new java.sql.Date(System.currentTimeMillis());
+	
+	@NotNull
+	private int visualizacao;
+	
+	@NotNull
+	private int curtida;
+	
+	@NotNull
+	@Size(min = 1, max = 500)
+	private String descricao;
+	
+	@NotNull
+	@Size(min = 1, max = 250)
+	private String comentario;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
