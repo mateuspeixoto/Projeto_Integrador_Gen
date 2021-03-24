f2ce304c0ef0ef586049df139e016219acee82f3
Beatriz Oliveira <beatrizlimaoliveira1999@gmail.com>
2021-03-24 14:37:36 -0300

    Criando componente logar

--- educaUS_front/educaUS/src/app/app-routing.module.ts
+++ educaUS_front/educaUS/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogarComponent } from './logar/logar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'entrar',component:LogarComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
