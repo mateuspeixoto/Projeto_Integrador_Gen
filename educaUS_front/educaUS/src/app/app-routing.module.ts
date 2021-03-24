418e71a9f029963f4c2c9adf0395744714dd8130
Beatriz Oliveira <beatrizlimaoliveira1999@gmail.com>
2021-03-24 18:15:19 -0300

    Criando o componente landing

--- educaUS_front/educaUS/src/app/app-routing.module.ts
+++ educaUS_front/educaUS/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 import { LogarComponent } from './logar/logar.component';
 
 const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
 
-  {path:'entrar',component:LogarComponent}
+  {path: 'home',component:LandingComponent},
+  {path: 'entrar',component:LogarComponent}
   
 ];
 
