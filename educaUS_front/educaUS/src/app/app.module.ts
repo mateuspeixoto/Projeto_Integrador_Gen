b062e2f24defc31326716421f82043f16857f404
unknown <alexsandro.15_sp@hotmail.com>
2021-03-25 11:33:37 -0300

    teste

--- educaUS_front/educaUS/src/app/app.module.ts
+++ educaUS_front/educaUS/src/app/app.module.ts
@@ -3,24 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { MenuComponent } from './menu/menu.component';
-=======
+
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LogarComponent } from './logar/logar.component';
 import { LandingComponent } from './landing/landing.component';
->>>>>>> 4841c6cbb021421cf619674f7409bb6c512fb492
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    MenuComponent
-=======
+    MenuComponent,
     CadastroComponent,
     LogarComponent,
     LandingComponent
->>>>>>> 4841c6cbb021421cf619674f7409bb6c512fb492
   ],
   imports: [
     BrowserModule,
