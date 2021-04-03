502e441dc315ae2d202e7695d3c3ada627f10f10
Gabriel Monteiro <brgabriel.monteiro@gmail.com>
2021-04-03 10:43:23 -0300

    teste

--- educaUS_front/educaUS/src/app/app-routing.module.ts
+++ educaUS_front/educaUS/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { from } from 'rxjs';
+
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { FeedComponent } from './feed/feed.component';
--- educaUS_front/educaUS/src/app/landing/landing.component.ts
+++ educaUS_front/educaUS/src/app/landing/landing.component.ts
@@ -12,9 +12,5 @@ export class LandingComponent implements OnInit {
   ngOnInit() {
     window.scroll(0,0)
   }
-<<<<<<< HEAD
   
-=======
-
->>>>>>> 391cb6403624bd6c96eeecc7fa222a2b70fd17e9
 }
